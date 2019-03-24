f94cd9b07d1bbdf034a428f94e4359cadedc34de
Artem Fedotov <hahahappyface@gmail.com>
2019-03-24 18:07:25 +0300

    feat: add function code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let result = false;
+  str = "*" + str + "+";
+      for (let i=1;(str.charAt(i)!="+");i++) {
+        for (let j=0;j<bracketsConfig.length;j++) {
+          if ((str.charAt(i) === bracketsConfig[j][0]) && (str.charAt(i+1) === bracketsConfig[j][1])) {
+            str = str.slice(0,i) + str.slice(i+2);
+            i = 0;
+            break;
+          }
+        }
+     }
+   if (str.localeCompare("*+") == 0) {
+    result = true;
+   }
+   return(result);
+  }
